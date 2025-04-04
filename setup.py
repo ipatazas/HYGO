77da7df99c4de658b40f3634bf62574914297900
Isaac Robledo <isaac.robledo.martin@gmail.com>
2025-04-04 11:47:39 +0200

    Version 0.9.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HYGO',
-    version='0.9.4',
+    version='0.9.5',
     description='HYGO: A genetic optimization framework',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
